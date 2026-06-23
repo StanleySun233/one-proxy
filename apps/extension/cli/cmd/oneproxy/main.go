4fd2e18c813ba7208459bb6aa9d75fab62e7a322
stanleysun233 <stanleysun233@163.com>
2026-06-23 22:35:32 +0800

    chore(release): align extension version 2.2.0

--- apps/extension/cli/cmd/oneproxy/main.go
+++ apps/extension/cli/cmd/oneproxy/main.go
@@ -14,7 +14,7 @@ import (
 	"github.com/StanleySun233/python-proxy/apps/extension/cli/internal/proxycommand"
 )
 
-const version = "2.1.24"
+const version = "2.2.0"
 const defaultNodeHTTPPort = 2988
 
 func main() {
